4c27f70c40c415d024387cc2a8285ba7fce1db5f
Alexandre Laranjeiras <laranja22@hotmail.com>
2022-05-16 01:27:20 -0300

    First commit

---
+++ IConfig.cs
@@ -0,0 +1,17 @@
+﻿using Amazon;
+
+namespace poc_sns
+{
+    internal interface IConfig
+    {
+        RegionEndpoint RegionEndpoint { get; }
+        string AccessKey { get; }
+        string SecretKey { get; }
+        
+        /// <summary>
+        /// Use +
+        /// Ex: +5521999999999
+        /// </summary>
+        string Number { get; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using poc_sns;
+
+var keys = new Config();
+
+string number = keys.Number;
+string message = "Hello at " + DateTime.Now.ToShortTimeString();
+
+BasicAWSCredentials awsCredentials = new BasicAWSCredentials(keys.AccessKey, keys.SecretKey);
+Amazon.RegionEndpoint awsRegion = keys.RegionEndpoint;
+
+var client = new AmazonSimpleNotificationServiceClient(credentials: awsCredentials, region: awsRegion);
+var request = new PublishRequest
+{
+    Message = message,
+    PhoneNumber = number
+};
+
+try
+{
+    await client.PublishAsync(request);
+    Console.WriteLine("Message sent to " + number + ":");
+    Console.WriteLine(message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Caught exception publishing request:");
+    Console.WriteLine(ex.Message);
+}
